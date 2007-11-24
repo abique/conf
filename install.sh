035cf230953e6d77b25e9f490d6b49a478ca7511
babali <babali@b15a5f19-c31c-dc11-9f3d-00188b745eb1>
2007-11-24 14:50:38 +0000

    removing .
    git-svn-id: http://tools.assembla.com/svn/conf_unix/babali@145 b15a5f19-c31c-dc11-9f3d-00188b745eb1

--- install.sh
+++ install.sh
@@ -6,31 +6,31 @@ CONF_PATH=`pwd`
 function install()
 {
     rm -rf ${HOME}/$1
-    ln -sf ${CONF_PATH}/$1 ${HOME}/$1
+    ln -sf ${CONF_PATH}/$1 ${HOME}/.$1
     printf "\e[33mInstalling\e[m ${HOME}/\e[32m$1\e[m\n"
 }
 
 [[ -d ${HOME}/.config ]] || mkdir ${HOME}/.config
 
 for file in		\
-    .bashrc		\
-    .bash.d             \
-    .beryl		\
-    .beryl-managerrc	\
-    .emacs		\
+    bashrc		\
+    bash.d              \
+    beryl		\
+    beryl-managerrc	\
+    emacs		\
     emacs.d		\
-    .fluxbox            \
-    .htoprc		\
-    .indent.pro         \
-    .inputrc		\
-    .toprc		\
-    .screenrc           \
-    .vimrc              \
-    .Xdefaults          \
-    .xinitrc		\
-    .xmodmaprc		\
-    .xsession		\
-    .config/Trolltech.conf \
+    fluxbox             \
+    htoprc		\
+    indent.pro          \
+    inputrc		\
+    toprc		\
+    screenrc            \
+    vimrc               \
+    Xdefaults           \
+    xinitrc		\
+    xmodmaprc		\
+    xsession		\
+    config/Trolltech.conf \
 
 do
     install ${file}
