1b7b60c8d54949faca43a43b7b79ada506cda8fd
babali <babali@b15a5f19-c31c-dc11-9f3d-00188b745eb1>
2008-06-28 12:48:19 +0000

    git-svn-id: http://tools.assembla.com/svn/conf_unix/babali@240 b15a5f19-c31c-dc11-9f3d-00188b745eb1

---
+++ bash.d/prompt2.bash
@@ -0,0 +1,33 @@
+function prompt_babali()
+{
+    RET=$?
+
+    if [[ $RET -eq 0 ]] ; then
+        RET=""
+    else
+        RET="\[$redB\]$RET "
+    fi
+
+    tmp=$(jobs)
+    if [[ ${#tmp} -gt 0 ]] ; then
+        JOBS="\[$yellow\]\j "
+    else
+        JOBS=""
+    fi
+
+    my_pwd=$(sed 's,\(/\?.\)[^/]*/,\1/,g' <<<"$PWD" )
+
+    PS1="$RET$JOBS\[$cyan\]\u@\H:\[$blue\]$my_pwd\[$green\]\$\[$white\]"
+
+# Change the window title of X terminals
+    case $TERM in
+	*xterm*|*rxvt*|Eterm)
+	    echo -ne "\033]0;${USER}@${HOSTNAME%%.*}:${PWD/$HOME/~}\007"
+	    ;;
+	screen)
+	    echo -ne "\033_${USER}@${HOSTNAME%%.*}:${PWD/$HOME/~}\033\\"
+	    ;;
+    esac
+}
+
+PROMPT_COMMAND="prompt_babali"
