b4dc2b90517444d6236bfb564a7a3cca0c43794e
babali <babali@b15a5f19-c31c-dc11-9f3d-00188b745eb1>
2009-02-28 21:52:11 +0000

    git-svn-id: http://tools.assembla.com/svn/conf_unix/babali@302 b15a5f19-c31c-dc11-9f3d-00188b745eb1

--- config/openbox/autostart.sh
+++ config/openbox/autostart.sh
@@ -2,6 +2,8 @@
 
 ~/conf/common_start.sh
 
-if which bmpanel 2>/dev/null >/dev/null ; then
+if which xfce4-panel 2>/dev/null >&2 ; then
+    xfce4-panel & disown
+elif which bmpanel 2>/dev/null >&2 ; then
     bmpanel & disown
 fi
