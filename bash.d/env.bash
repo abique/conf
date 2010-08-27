d9d2734a0ba86b7cac87dd4c1836897d4b4a5ee9
babali <babali@b15a5f19-c31c-dc11-9f3d-00188b745eb1>
2010-08-27 14:27:20 +0000

    shopt -s checkwinsize
    git-svn-id: http://tools.assembla.com/svn/conf_unix/babali@434 b15a5f19-c31c-dc11-9f3d-00188b745eb1

--- bash.d/env.bash
+++ bash.d/env.bash
@@ -51,3 +51,7 @@ export NAME="Alexandre Bique"
 export DEBFULLNAME="Alexandre Bique"
 
 #export CSCOPE_LINEFLAG_AFTER_FILE=no
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
