39f0f261e1a3c8f87831c742586757cc5c1ab68d
babali <babali@b15a5f19-c31c-dc11-9f3d-00188b745eb1>
2011-04-10 11:09:27 +0000

    xset b off
    git-svn-id: http://tools.assembla.com/svn/conf_unix/babali@454 b15a5f19-c31c-dc11-9f3d-00188b745eb1

--- common_start_no_wallpaper.sh
+++ common_start_no_wallpaper.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 
-which numlockx && numlockx
+which numlockx >/dev/null 2>/dev/null && numlockx
 setxkbmap -model evdev -layout us -variant altgr-intl -option -option ctrl:nocaps
 xrdb -load ~/.Xdefaults
-xset -b off
+xset b off
 xset r rate 175 30
 
 # nm-applet & disown
