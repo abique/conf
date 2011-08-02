af36370dba5bb79df70df493f7b861e98f745dbc
babali <babali@b15a5f19-c31c-dc11-9f3d-00188b745eb1>
2011-08-02 19:47:50 +0000

    Updated common_start.sh
    git-svn-id: http://tools.assembla.com/svn/conf_unix/babali@479 b15a5f19-c31c-dc11-9f3d-00188b745eb1

--- common_start.sh
+++ common_start.sh
@@ -4,3 +4,8 @@ if [[ -r ~/wallpapers/current ]] ; then
     feh --bg-scale ~/wallpapers/current
 fi
 . ~/conf/common_start_no_wallpaper.sh
+psi & disown
+nm-applet & disown
+xfce4-notifyd & disown
+kmix & disown
+ibus-daemon --replace & disown
