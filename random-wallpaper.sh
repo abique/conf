86173bebc3fb2a510845853b101d4f08a2a02252
Alexandre Bique <bique.alexandre@gmail.com>
2011-08-14 01:36:27 +0200

    Added a random-wallpaper script

---
+++ random-wallpaper.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+files=`find ~/wallpapers/ -name '*.jpg' -o -name '*.jpeg' -o -name '*.png'`
+IFS='
+'
+
+count=0
+for file in $files
+do
+    count=$(($count + 1))
+done
+
+select=$(($RANDOM % $count))
+i=0
+for file in $files
+do
+    if [ $i -eq $select ] ; then
+	echo Setting "$file"
+        feh --bg-scale "$file"
+	exit 0
+    fi
+    i=$(($i + 1))
+done
