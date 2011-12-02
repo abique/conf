8a457c3a087a6617af523b55221cd597512bd774
Alexandre Bique <bique.alexandre@gmail.com>
2011-12-02 06:55:30 +0100

    Don't start applications at startup

--- common_start.sh
+++ common_start.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
 
-~/conf/random_wallpaper.sh
+. ~/conf/random_wallpaper.sh
 . ~/conf/common_start_no_wallpaper.sh
-psi & disown
-xfce4-notifyd & disown
-kmix & disown
--- common_start_no_wallpaper.sh
+++ common_start_no_wallpaper.sh
@@ -5,3 +5,9 @@ setxkbmap -model evdev -layout us -variant altgr-intl -option -option ctrl:nocap
 xrdb -load ~/.Xdefaults
 xset b off
 xset r rate 175 30
+
+# apps
+xfce4-notifyd & disown
+if which nm-applet ; then
+  nm-applet & disown
+fi
