a78ff3c927ed139114a79adb3ecc6d8241525876
Alexandre Bique <abique@freebox.fr>
2012-03-16 18:07:38 +0100

    emacs: yasnippet

---
+++ emacs.d/snippet.el
@@ -0,0 +1,4 @@
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/yas")
+(require 'yasnippet) ;; not yasnippet-bundle
+(yas/initialize)
+(yas/load-directory "/usr/share/emacs/site-lisp/yas/snippets")
