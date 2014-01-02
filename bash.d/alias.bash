bb75794a88547835a4d20ba5a6f7247dcb5b98ef
Alexandre Bique <bique.alexandre@gmail.com>
2014-01-02 23:01:46 +0100

    x

--- bash.d/alias.bash
+++ bash.d/alias.bash
@@ -46,7 +46,7 @@ alias stats='~/develop/crawler-2/build/src/twenga/BinUtils/get-stats --host '
 
 alias debian='sudo chroot /local/debian /bin/bash -c "su abique --shell=/bin/bash"'
 alias reaper='cd ~/.wine/drive_c/Program\ Files/REAPER\ \(x64\)/ && wine reaper.exe'
-alias ableton='cd ~/.wine/drive_c/users/Public/Application\ Data/Ableton/Live\ 9\ Trial/Program/ && wine Ableton\ Live\ 9\ Trial.exe'
+alias ableton='cd ~/.wine/drive_c/users/Public/Application\ Data/Ableton/Live\ 9\ Suite/Program/ && wine Ableton\ Live\ 9\ Suite.exe'
 alias diablo3='cd ~/.wine/drive_c/Program\ Files\ \(x86\)/Diablo\ III/ && setarch i386 -3 -L -B -R wine Diablo\ III.exe -launch -opengl'
 
 function ssh_vnc_tunnel()
